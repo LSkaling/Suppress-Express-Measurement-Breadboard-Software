f86144cb55672512b081fb8b525d6a7faee1fe2a
beeenls <ben.lees@outlook.com>
2025-02-10 23:02:48 -0800

    Start MC connection in UI

--- RF24_Transmit/interface.py
+++ RF24_Transmit/interface.py
@@ -1,11 +1,15 @@
 import serial
+import serial.tools.list_ports
 from tkinter import *
 from tkinter.simpledialog import askfloat
 import tkinter.font as tkFont
 
+pad_x = 10
+pad_y = 10
+
 # Initial interface setup
 root = Tk()
-root.geometry("400x200")
+root.geometry("400x600")
 root.title("Measurement Station")
 default_font = tkFont.nametofont("TkDefaultFont")
 default_font.configure(family='Courier', size=18)
@@ -14,53 +18,72 @@ text_font.configure(family='Courier', size=18)
 frame = Frame(root)
 frame.pack()
 
-tray = Frame(root)
-tray.pack(side=TOP)
+# Connection form
+connect_panel = Frame(root)
+connect_panel.pack(side=TOP)
+ports = []
+ser = serial.Serial()
+ser.baudrate = 115200
 
-pad_x = 10
-pad_y = 10
+def port_scan():
+    global ports
+    ports_list.delete(0, END)
+    ports = serial.tools.list_ports.comports()
+    for i in range(len(ports)):
+        ports_list.insert("end", ports[i].name)
 
-# Serial start (TODO: take address as program argument)
-ser = serial.Serial('/dev/tty.usbmodem155895701')
+def port_select(event):
+    global ser
+    index = event.widget.curselection()[0]
+    ser.port = ports[index].name
+    readout.pack()
+    connect_panel.pack_forget()
 
-# Sensor stuff
-class Sensor:
-    zero = 0
-    calib = 1
-    reading = 0
-    weight = 0
-    T = int(1000 / 80) # sampling interval
+connect_label = Label(connect_panel, text="Not connected")
+ports_list = Listbox(connect_panel)
+ports_list.bind('<<ListboxSelect>>', port_select)
+scan_button = Button(connect_panel, text="Scan ports", command=port_scan)
+connect_label.pack()
+ports_list.pack()
+scan_button.pack()
 
-    def tare(self):
-        if self.calib != 1: # Zero in grams
-            self.zero = self.weight
+# Data readout display
+readout = Frame(root)
+readout.pack()
 
-        if self.calib == 1: # Zero in arbitrary units
-            self.zero = self.reading
+zero_all_button = Button(readout, text = "Zero all")
+zero_all_button.pack()
 
-    def calibrate(self):
-        self.calib = askfloat("Calibrate sensor", "Current weight on sensor") / self.weight
-        self.zero = 0
+# TODO: Learn node IDs and calibration from output node
+node_zeroes = []
+node_calibs = []
 
-    def update_measurement(self):
-        bytes = ser.readline()
-        bytes = bytes.decode("utf-8")
-        self.reading = float(str(bytes[0:-2])) # strip newlines (CRLF) and convert
-        self.weight = (self.reading * self.calib) - self.zero
-        txt = "Weight: " + "{:.1f}".format(self.weight) + "g"
-        measurement.config(text = txt)
-        root.after(self.T, self.update_measurement)
+# Sub-panels for individual nodes: ID label, CL display, calibrate switch
+node_panels = []
+node_labels = []
+node_cls = []
+node_switches_calib = []
 
-# Single sensor display
-sens0 = Sensor()
-button1 = Button(tray, text = "Zero", command = sens0.tare)
-button1.pack(padx = pad_x, pady = pad_y)
-button2 = Button(tray, text = "Calibrate", command = sens0.calibrate)
-button2.pack(padx = pad_x, pady = pad_y)
+node_panels.append(Frame(readout))
+node_labels.append(Label(node_panels[-1], text = "Sensor 1"))
+node_cls.append(Canvas(node_panels[-1], width = 50, height = 50))
+node_switches_calib.append(Button(node_panels[-1], text = "Calibrate"))
 
-measurement = Label(frame)
-measurement.pack()
+for i in range(len(node_panels)):
+    node_panels[i].pack()
+    node_labels[i].pack()
+    node_cls[i].pack()
+    node_switches_calib[i].pack()
 
-# Start loop and update callbacks
-sens0.update_measurement()
+readout.pack_forget()
 root.mainloop()
+
+#    def update_measurement(self):
+#        bytes = ser.readline()
+#        bytes = bytes.decode("utf-8")
+#        self.reading = float(str(bytes[0:-2])) # strip newlines (CRLF) and convert
+#        self.weight = (self.reading * self.calib) - self.zero
+#        txt = "Weight: " + "{:.1f}".format(self.weight) + "g"
+#        measurement.config(text = txt)
+#        root.after(self.T, self.update_measurement)
+
